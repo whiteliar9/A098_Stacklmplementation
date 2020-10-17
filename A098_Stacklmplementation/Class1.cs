4f95c580d93a6cc201290f664aba8dcef1ac229a
whitelior <whiteliar0505@gmail.com>
2020-10-17 12:54:03 +0900

    초기 커밋.

---
+++ A098_Stacklmplementation/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A098_Stacklmplementation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.WriteLine("Push(" + val + ") ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+                Console.Write("Pop()=" + stack.Pop() + ",");
+        }
+    }
+}
